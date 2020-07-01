258d48173f72d4e7775372d467bc2f8cf0272926
Alex Bates <ard.bates@gmail.com>
2020-07-01 22:28:32 +0100

    Remove run.nix commented code

--- run.nix
+++ run.nix
@@ -3,14 +3,3 @@
 pkgs.mkShell {
   buildInputs = [pkgs.xorriso pkgs.qemu];
 }
-
-# let
-#   qemuGtk = pkgs.qemu.override (oldAttrs: rec {
-#     gtkSupport = true;
-#   });
-# in
-#   pkgs.mkShell {
-#     buildInputs = [
-#       pkgs.xorriso qemuGtk
-#     ];
-#   }
